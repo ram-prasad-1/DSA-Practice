9069236ed238b824a2e9514da2db9228eab36af7
ram-prasad-1 <ramprasaddeora@gmail.com>
2021-12-16 14:13:00 +0530

    some changes

--- src/basics/fibonacci-num.js
+++ src/basics/fibonacci-num.js
@@ -1,6 +1,6 @@
 `
 F(n) = F(n-1) + F(n-2)
-Time complexity: O(2^n) 
+Time complexity: O(2^n)
 See: https://stackoverflow.com/a/45618079/3565194
 `;
 
@@ -9,13 +9,14 @@ function getFibNum(n) {
   return getFibNum(n - 1) + getFibNum(n - 2);
 }
 
-// optimized
-function getFibNum2(n) {
+function getFibNumOpimized(n) {
   const memory = [0, 1];
 
   for (let i = 2; i <= n; i++) {
     memory[i] = memory[i - 1] + memory[i - 2];
   }
+
+  return memory[n];
 }
 
-console.log(getFibNum(7));
+console.log(getFibNumOpimized(7));
