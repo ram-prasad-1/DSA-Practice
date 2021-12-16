43d74296cdd8fe378bd33f8cb354ac81d4296632
ram-prasad-1 <ramprasaddeora@gmail.com>
2021-12-16 13:44:55 +0530

    some changes

--- src/basics/fibonacci-num.js
+++ src/basics/fibonacci-num.js
@@ -1,1 +1,13 @@
-// time complexity of a recursive approach
+
+`
+F(n) = F(n-1) + F(n-2)
+Time complexity: O(2^n) 
+See: https://stackoverflow.com/a/45618079/3565194
+`
+function getFibNum(n) {
+    if (n <= 1) return n;
+    return getFibNum(n-1) + getFibNum(n-2)
+}
+
+
+console.log(getFibNum(7))
---
+++ src/sorting-and-searching/binary-search.js
@@ -0,0 +1,6 @@
+
+`
+https://www.programiz.com/dsa/binary-search
+Time Complexity: O(log(n))
+See: https://stackoverflow.com/a/8185382/3565194
+`
