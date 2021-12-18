fe6780e3093e5c44027595bf9694c3600a9fd8ce
ram-prasad-1 <ramprasaddeora@gmail.com>
2021-12-18 12:11:02 +0530

    add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  name: 'dsa-practice.js',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/coverage/'],
+};
