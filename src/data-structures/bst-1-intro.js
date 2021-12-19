acdc521dd31cd64dde6975f0e89611de48fa6be3
ram-prasad-1 <ramprasaddeora@gmail.com>
2021-12-19 11:19:05 +0530

    some changes

--- src/data-structures/bst-1-intro.js
+++ src/data-structures/bst-1-intro.js
@@ -1,9 +1,8 @@
-`
+const { BaseBinarySearchTree, Node } = require('./helpers/base-bst');
 
-`;
-
-class BinarySearchTree {
+class BinarySearchTree extends BaseBinarySearchTree {
   constructor() {
+    super();
     this.root = null;
   }
 
@@ -16,17 +15,17 @@ class BinarySearchTree {
   }
 
   // trick: start with plain slate. now insert existing with new one as well.
-  _insert(currentNode, newValue) {
-    if (currentNode === null) {
+  _insert(node, newValue) {
+    if (node === null) {
       return new Node(newValue);
     }
 
-    if (newValue <= currentNode.value) {
-      currentNode.left = this._insert(currentNode.left, newValue);
+    if (newValue <= node.value) {
+      node.left = this._insert(node.left, newValue);
     } else {
-      currentNode.right = this._insert(currentNode.right, newValue);
+      node.right = this._insert(node.right, newValue);
     }
-    return currentNode;
+    return node;
   }
 
   search(value) {
@@ -76,87 +75,6 @@ class BinarySearchTree {
     }
   }
 
-  prettyPrint() {
-    const trioArray = this.getTrioPerNodes();
-    if (trioArray.length === 0) {
-      console.log(null);
-    }
-
-    let rootIndex = -1;
-    for (const [index, trio] of trioArray.entries()) {
-      trio[0] = trio[0] || ' ';
-      trio[2] = trio[2] || ' ';
-
-      if (this.root.value === trio[1]) {
-        rootIndex = index;
-      }
-    }
-
-    const PADDING = 8;
-    const TRIO_LENGTH = 10;
-    const getSpaces = count => [...Array(count)].map(() => ' ').join('');
-    const withFilledSpaces = trioStr => {
-      if (trioStr.length < TRIO_LENGTH) {
-        trioStr += getSpaces(TRIO_LENGTH - trioStr.length);
-      }
-      return trioStr;
-    };
-
-    let left = rootIndex - 1;
-    let right = rootIndex + 1;
-
-    let str =
-      getSpaces(PADDING) + withFilledSpaces(`[${trioArray[rootIndex]}]`) + getSpaces(PADDING);
-    let rowStr = '';
-
-    while (left >= 0 || right < trioArray.length) {
-      if (left >= 0) {
-        rowStr = withFilledSpaces(`[${trioArray[left]}]`);
-        left -= 1;
-      }
-      rowStr += getSpaces(PADDING);
-      if (right < trioArray.length) {
-        rowStr += withFilledSpaces(`[${trioArray[right]}]`);
-        right += 1;
-      }
-
-      str += '\n' + rowStr;
-    }
-
-    console.log(str);
-  }
-
-  getTrioPerNodes() {
-    const trioArray = [];
-    this._getTrioPerNodes(this.root, trioArray);
-    trioArray.sort((a, b) => {
-      if (trioArray.length <= 1) {
-        return 1;
-      }
-      return a[1] - b[1];
-    });
-    return trioArray;
-  }
-
-  _getTrioPerNodes(node, arr) {
-    if (node === null) return null;
-    const trio = [null, null, null];
-
-    if (node.left) {
-      trio[0] = this._getTrioPerNodes(node.left, arr);
-    }
-
-    trio[1] = node.value;
-
-    if (node.right) {
-      trio[2] = this._getTrioPerNodes(node.right, arr);
-    }
-    if (node.left !== null || node.right !== null) {
-      arr.push(trio);
-    }
-    return node.value;
-  }
-
   deleteNode(value) {
     this._deleteNode(this.root, value);
   }
@@ -164,15 +82,7 @@ class BinarySearchTree {
   _deleteNode(node, value) {}
 }
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
 module.exports = {
-  Node,
   BinarySearchTree,
+  Node,
 };
---
+++ src/data-structures/helpers/base-bst.js
@@ -0,0 +1,97 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BaseBinarySearchTree {
+  constructor() {}
+
+  prettyPrint() {
+    const trioArray = this.getTrioPerNodes();
+    if (trioArray.length === 0) {
+      console.log(null);
+    }
+
+    let rootIndex = -1;
+    for (const [index, trio] of trioArray.entries()) {
+      trio[0] = trio[0] || ' ';
+      trio[2] = trio[2] || ' ';
+
+      if (this.root.value === trio[1]) {
+        rootIndex = index;
+      }
+    }
+
+    const PADDING = 8;
+    const TRIO_LENGTH = 10;
+    const getSpaces = count => [...Array(count)].map(() => ' ').join('');
+    const withFilledSpaces = trioStr => {
+      if (trioStr.length < TRIO_LENGTH) {
+        trioStr += getSpaces(TRIO_LENGTH - trioStr.length);
+      }
+      return trioStr;
+    };
+
+    let left = rootIndex - 1;
+    let right = rootIndex + 1;
+
+    let str =
+      getSpaces(PADDING) + withFilledSpaces(`[${trioArray[rootIndex]}]`) + getSpaces(PADDING);
+    let rowStr = '';
+
+    while (left >= 0 || right < trioArray.length) {
+      if (left >= 0) {
+        rowStr = withFilledSpaces(`[${trioArray[left]}]`);
+        left -= 1;
+      }
+      rowStr += getSpaces(PADDING);
+      if (right < trioArray.length) {
+        rowStr += withFilledSpaces(`[${trioArray[right]}]`);
+        right += 1;
+      }
+
+      str += '\n' + rowStr;
+    }
+
+    console.log(str);
+  }
+
+  getTrioPerNodes() {
+    const trioArray = [];
+    this._getTrioPerNodes(this.root, trioArray);
+    trioArray.sort((a, b) => {
+      if (trioArray.length <= 1) {
+        return 1;
+      }
+      return a[1] - b[1];
+    });
+    return trioArray;
+  }
+
+  _getTrioPerNodes(node, arr) {
+    if (node === null) return null;
+    const trio = [null, null, null];
+
+    if (node.left) {
+      trio[0] = this._getTrioPerNodes(node.left, arr);
+    }
+
+    trio[1] = node.value;
+
+    if (node.right) {
+      trio[2] = this._getTrioPerNodes(node.right, arr);
+    }
+    if (node.left !== null || node.right !== null) {
+      arr.push(trio);
+    }
+    return node.value;
+  }
+}
+
+module.exports = {
+  Node,
+  BaseBinarySearchTree,
+};
