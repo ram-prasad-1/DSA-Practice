9fe2e2e935a60887d0f6ec75c82832be0f670c4b
ram-prasad-1 <ramprasaddeora@gmail.com>
2021-12-25 16:25:20 +0530

    some changes

---
+++ src/interview-handbook/string/5-longest-palindromic-substring.js
@@ -0,0 +1,38 @@
+`
+Expand Around Center Approach: O(n^2)
+
+Manacher’s Algorithm --> O(n):
+https://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-1/
+`;
+const longestPalindrome = function (str) {
+  if (str.length <= 1) return str;
+  let maxL = 0;
+  let maxR = 0;
+
+  for (let i = 1; i < str.length - 1; i = i + 1 / 2) {
+    let pL = -1;
+    let pR = -1;
+
+    let left = Math.floor(i);
+    let right = Math.ceil(i);
+    while (left >= 0 && right <= str.length - 1) {
+      if (str[left] !== str[right]) {
+        break;
+      }
+      pL = left;
+      pR = right;
+
+      left -= 1;
+      right += 1;
+    }
+
+    if (pR - pL > maxR - maxL) {
+      maxR = pR;
+      maxL = pL;
+    }
+  }
+
+  return str.slice(maxL, maxR + 1);
+};
+
+console.log(longestPalindrome('atoantabccccbantaotano'));
