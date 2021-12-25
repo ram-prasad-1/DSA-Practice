e90afe02d14d038c7faf432756a14e4ce06f451c
ram-prasad-1 <ramprasaddeora@gmail.com>
2021-12-25 20:11:19 +0530

    some changes

--- src/algorithms/algorithmic-techniques/knapsack-problem.js
+++ src/algorithms/algorithmic-techniques/knapsack-problem.js
@@ -21,8 +21,31 @@ function getMaxValue(weights, values, weightCapacity) {
   return _getMaxValue(0, weightCapacity);
 }
 
+// Solution 2: backtracking
+function getMaxValue2(weights, values, weightCapacity) {
+  let maxValue = Number.MIN_SAFE_INTEGER;
+  const _getMaxValue2 = (path, start) => {
+    const wtSum = path.reduce((agg, current) => agg + weights[current], 0);
+    if (wtSum <= weightCapacity) {
+      maxValue = Math.max(
+        maxValue,
+        path.reduce((agg, current) => agg + values[current], 0)
+      );
+    } else {
+      return;
+    }
+    for (let i = start; i < weights.length; i++) {
+      path.push(i);
+      _getMaxValue2(path, i + 1);
+      path.pop();
+    }
+  };
+  _getMaxValue2([], 0);
+  return maxValue;
+}
+
 const weights = [4, 2, 1, 10, 2];
 const values = [12, 2, 1, 4, 1];
 const capacity = 15;
 
-console.log(getMaxValue(weights, values, capacity));
+console.log(getMaxValue2(weights, values, capacity));
