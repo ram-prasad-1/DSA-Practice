25697261f3c28120d8583161c2f5c55229cffdf5
ram-prasad-1 <ramprasaddeora@gmail.com>
2021-12-26 10:44:07 +0530

    some changes

--- src/algorithms/dynamic-programming/416-partition-equal-subset-sum.js
+++ src/algorithms/dynamic-programming/416-partition-equal-subset-sum.js
@@ -1,5 +1,23 @@
-// backtracking
 function canPartition(arr) {
+  const sum = arr.reduce((agg, curr) => agg + curr, 0);
+  const halfSum = sum / 2;
+  if (sum % 2 === 1) return false;
+
+  // just check if halfSum is possible or not
+  return _canPartition(arr, 0, halfSum);
+}
+
+function _canPartition(arr, i, sum) {
+  if (i >= arr.length - 1) return false;
+  if (sum === arr[i]) return true;
+
+  const withInclusion = _canPartition(arr, i + 1, sum - arr[i]);
+  const withExclusion = _canPartition(arr, i + 1, sum);
+  return withInclusion || withExclusion;
+}
+
+// backtracking
+function canPartition2(arr) {
   const sum = arr.reduce((agg, curr) => agg + curr, 0);
   const halfSum = sum / 2;
   if (sum % 2 === 1) return false;
@@ -21,5 +39,3 @@ function canPartition(arr) {
   };
   return fn([], arr);
 }
-
-console.log(canPartition([1, 5, 11, 5, 4, 12, 8]));
