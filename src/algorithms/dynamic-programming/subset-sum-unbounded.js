884c532ed3f1ef692cd63af89e6ce7775dfd889d
ram-prasad-1 <ramprasaddeora@gmail.com>
2021-12-27 18:39:54 +0530

    some changes

---
+++ src/algorithms/dynamic-programming/subset-sum-unbounded.js
@@ -0,0 +1,24 @@
+`
+find the subset of an array of whole numbers with sum equal to a given target when array element can repeat themselves
+https://www.youtube.com/watch?v=jgps7MXtKRQ
+`;
+
+function isScorePossible(arr, targetScore) {
+  // const scores = Array(targetScore + 1).fill(false);
+  // scores[0] = true; // by default zero score
+  //
+  // // Trick: keep on building optimal substructures starting with smallest set
+  // for (let i = 0; i < arr.length; i++) {
+  //   for (let j = 0; j < scores.length; j++) {
+  //     const val = arr[i];
+  //     if (j >= val) {
+  //       // checks both the cases: 1. item included and item 2. item excluded
+  //       scores[j] = scores[j - val] || scores[j];
+  //     }
+  //   }
+  // }
+  //
+  // return scores[scores.length - 1];
+}
+
+console.log(isScorePossible([6, 3, 2, 4], 7));
