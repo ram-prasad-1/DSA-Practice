a607a40d0f3d27e523a714cc3cd48b86402d574c
ram-prasad-1 <ramprasaddeora@gmail.com>
2022-01-02 15:09:48 +0530

    some changes

--- src/algorithms/dynamic-programming/climbing-stairs.js
+++ src/algorithms/dynamic-programming/climbing-stairs.js
@@ -1,10 +1,9 @@
 function climbStairs(n) {
   const arr = Array(n + 1).fill(null);
-  arr[0] = 0;
   arr[1] = 1;
   arr[2] = 2;
   for (let i = 3; i <= n; i++) {
-    arr[i] = 1 * arr[i - 1] + 2 * arr[i - 2];
+    arr[i] = arr[i - 1] + arr[i - 2];
   }
   return arr[n];
 }
