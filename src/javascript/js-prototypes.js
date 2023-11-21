4eb3d6881e2060b06087a13c514ea010dde9bf15
ram-prasad-1 <ramprasaddeora@gmail.com>
2023-11-21 10:00:08 +0530

    few changes

--- src/javascript/js-prototypes.js
+++ src/javascript/js-prototypes.js
@@ -2,7 +2,11 @@
 obj.__proto__ ⇒ Prototype object of obj
 Fn.prototype ⇒ (only for function objects) Used to set __proto__ for new Fn objects.
 
-obj.__proto__ = Fn.prototype; and so on
+const obj = new Fn();
+obj.__proto__ === Fn.prototype; // true
+
+
+// Fn.prototype is just a regular property, by default = { constructor };
 `;
 
 const root = {
