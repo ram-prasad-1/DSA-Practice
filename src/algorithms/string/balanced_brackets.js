34da4623e69ced20a8ae21c43534e41f6882a76d
ram-prasad-1 <ramprasaddeora@gmail.com>
2024-01-22 08:54:24 +0530

    new changes

---
+++ src/algorithms/string/balanced_brackets.js
@@ -0,0 +1,8 @@
+`
+Check for Balanced Brackets in an expression (well-formedness)
+
+Trick: The idea is to put all the opening brackets in the stack.
+Whenever you hit a closing bracket, search if the top of the stack
+is the opening bracket of the same nature.
+https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
+`;
