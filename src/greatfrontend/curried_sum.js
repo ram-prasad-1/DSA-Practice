cdb14a66571911eb95e02b3e7d5a75886a5592bc
ram-prasad-1 <ramprasaddeora@gmail.com>
2024-03-10 03:47:35 +0530

    few changes

--- src/greatfrontend/curried_sum.js
+++ src/greatfrontend/curried_sum.js
@@ -6,7 +6,7 @@ https://www.greatfrontend.com/questions/javascript/curry
 Qn. sum(4)(3)(7)('.') // returns 14
 `;
 
-// Trick: always return the nested function.
+// Trick: always return the inner function.
 function sum(a) {
   let total = a;
   const _sumInner = (b) => {
